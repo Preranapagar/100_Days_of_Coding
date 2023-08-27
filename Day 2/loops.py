6f9b03371b8c4f834fdc41403d6af762a48addb0
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-08-27 14:19:03 +0530

    loops demo

---
+++ Day 2/loops.py
@@ -0,0 +1,24 @@
+
+#loops in python
+
+#for loop
+
+print("loop demo 1:")
+for i in range(5):
+    print(i)
+
+
+print("loop demo 2:")
+for i in range(5,10):
+    print(i)
+
+print("loop demo 3:")
+for i in range(1,20,2):
+    print(i)
+
+#while loop
+print("demo of while loop:")
+i = 5
+while (i<10):
+    print(i)
+    i = i+1
