f03a33c451e99b004a86b0acc923492f8b8b69c3
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-08-27 20:07:28 +0530

    Create Function7.py
    Inner function

---
+++ Day 2/Function7.py
@@ -0,0 +1,21 @@
+#function accepts parameter as another function
+
+def Add(A,B):
+    return A + B
+
+def Sub(A,B):
+    return A - B
+
+def Marvellous(FPTR1, FPTR2):
+    Ans = FPTR1(10,4)
+    print(type(FPTR1))
+    print("Addition is :", Ans)
+
+    Ans = FPTR2(10,4)
+    print("Substraction is :", Ans)
+
+def main():
+    Marvellous(Add,Sub)
+
+if __name__ == "__main__":
+    main()
