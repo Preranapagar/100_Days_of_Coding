8ec0dc98f65405cb6cc6745fc61745c84e41c8a9
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-08-27 19:22:18 +0530

    Update Function3.py
    function which returns multiple parameters

--- Day 2/Function3.py
+++ Day 2/Function3.py
@@ -1,1 +1,18 @@
 #Function accept multiple parameter and returns multiple parameter
+
+def Marvellous(Value1, Value2):
+    Addition = Value1 + Value2
+    Substraction = Value1 - Value2
+    Multiplication = Value1 * Value2
+
+    return Addition , Substraction , Multiplication
+
+def main():
+    Ret1, Ret2, Ret3 = Marvellous(3,4)
+
+    print("Addition is :", Ret1)
+    print("Substraction is :", Ret2)
+    print("Multiplication is :", Ret3)
+
+if __name__ == "__main__":
+    main()
