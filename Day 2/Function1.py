089a70af1251b1212f8f3e927a965655172e2072
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-08-27 19:08:51 +0530

    Create Function1.py
    function

---
+++ Day 2/Function1.py
@@ -0,0 +1,14 @@
+#Function 
+
+def Marvellous(Name,Age,City):
+    print("Inside Marvellous Function")
+    print("Welcome", Name)
+    print("Your age is :", Age)
+    print("Your City is :", City)
+
+def main():
+    Marvellous("Amit",28,"Pune")
+    
+
+if __name__ == "__main__":
+    main()
