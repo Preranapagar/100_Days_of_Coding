90c633a7471562b0c23473916cb73c7b2a0f2d3d
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-02 18:18:52 +0530

    datetime module

---
+++ Day 6/datetime_module.py
@@ -0,0 +1,30 @@
+
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+
+Today = datetime.today()
+print("Today :", Today)
+print(type(Today))
+
+Today_date = date.today()
+print("Today's date :", Today_date)
+print(type(Today_date))
+
+print("Month :", Today_date.month)
+print("Year :", Today_date.year)
+print("Day :", Today_date.day)
+
+Chritsmas = date(Today_date.year, 12, 25)
+print("Chritsmas Day :", Chritsmas)
+
+#number of days remain for Chritsmas
+
+days_to_christmas = (Chritsmas - Today_date).days
+
+print("Number of days for Chritsmas :", days_to_christmas)
+
+eta = timedelta(days = 2, hours = 3)
+print(Today)
+result = Today + eta
+print(result)
