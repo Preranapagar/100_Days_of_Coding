047b3cb01f070e4adc293c5381f830bdb49fd627
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-09 20:43:20 +0530

    recursion
    getting recursion limit

--- Day 8/recursion2.py
+++ Day 8/recursion2.py
@@ -2,6 +2,10 @@ import sys
 
 def main():
     print("Recursion Limit :", sys.getrecursionlimit())
+    sys.setrecursionlimit(1500)
+    print("Recursion Limit :", sys.getrecursionlimit())
+
+
 
 if __name__=="__main__":
     main()
