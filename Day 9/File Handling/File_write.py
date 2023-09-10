7b789a422bf8390eac87bfb85cccb80d69a6beda
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-10 19:28:15 +0530

    File Handling
    writing file

---
+++ Day 9/File Handling/File_write.py
@@ -0,0 +1,23 @@
+import os
+
+def main():
+    print("Enter the name of the file that you want to open for writing purpose:")
+    File_Name = input()
+
+    if os.path.exists(File_Name):
+        fobj = open(File_Name,"a") #append mode
+        if fobj:
+            print("File successfully open in append mode ")
+
+            print("Enter your data :")
+            Data = input()
+            fobj.write(Data) #write the data into file
+
+            fobj.close()
+        else:
+            print("Unable to open the file")
+    else:
+        print("There is no such files")
+
+if __name__=="__main__":
+    main()
