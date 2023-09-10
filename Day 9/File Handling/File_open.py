bac670bc144dbddb3529d7fa6d411251518c1d59
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-10 19:28:43 +0530

    File Handling
    File opening

---
+++ Day 9/File Handling/File_open.py
@@ -0,0 +1,18 @@
+import os
+
+def main():
+    print("Enter the name of the file that you want to open for reading purpose:")
+    File_Name = input()
+
+    if os.path.exists(File_Name):
+        fobj = open(File_Name,"r")
+        if fobj:
+            print("File open successfully")
+            fobj.close()
+        else:
+            print("Unable to open the file")
+    else:
+        print("There is no such files")
+
+if __name__=="__main__":
+    main()
