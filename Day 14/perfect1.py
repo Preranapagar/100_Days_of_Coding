4e8c391abb779657a11d4a20f401338e8ccae64a
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-15 19:47:06 +0530

    Create perfect1.py
    perfect num

---
+++ Day 14/perfect1.py
@@ -0,0 +1,35 @@
+#check perfect number using while loop
+
+def CheckPerfect(Num):
+    if Num <= 1:
+        return False
+    
+    div_sum = 1
+    i = 2
+
+    while i*i <= Num:
+        
+        if Num % i == 0:
+            div_sum += i
+            if i != Num//i:
+                div_sum += Num//i
+            print("i :", i)
+    
+        i += 1
+
+    if div_sum == Num:
+        return True
+    else:
+        return False
+    
+def main():
+
+    No = int(input("Enter the number to check :"))
+
+    Answer = CheckPerfect(No)
+    print(Answer)
+
+if __name__=="__main__":
+    main()
+    
+
