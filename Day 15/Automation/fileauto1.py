644d1be8c0770cc6b081d9245d8894cd9ef1a577
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-16 18:01:22 +0530

    Update fileauto1.py

--- Day 15/Automation/fileauto1.py
+++ Day 15/Automation/fileauto1.py
@@ -1,29 +1,35 @@
 from sys import *
+import os
 
+def DirectoyTravel(DirName):
+    print("We are going to scan the Directory :", DirName)
+
+    for foldername, subfoldername, filename in os.walk(DirName):
+        for fname in filename:
+            print(fname)
 
 def main():
     print("_______________Automation Script_______________")
     print("Automation Script Name :", argv[0])
 
-    if (len(argv)==2):
-        if (argv[1] == "-h" or argv[1]== "-H"): #flag for displaying help
-            print("This automation script is used to perform file automation")
-            exit()
-
-        elif (argv[1] == "-u" or argv[1]=="-U"): #flag displaying the usage of script
-            print("Usage : Name_of_script First_Argument")
-            print("Example : Demo.py Marvellous")
-            exit()
-
-        else:
-            print("There is no such optionn to handle")
-
     if(len(argv) !=2):
         print("Invalid Number of Aurguments")
         exit()
 
+    if (argv[1] == "-h" or argv[1]== "-H"): #flag for displaying help
+        print("This automation script is used to perform file automation")
+        exit()
+
+    elif (argv[1] == "-u" or argv[1]=="-U"): #flag displaying the usage of script
+        print("Usage : Name_of_script First_Argument")
+        print("Example : Demo.py Marvellous")
+        exit()
+    
     else:
-        pass
+        DirectoyTravel(argv[1])
+        #logic
+        
+
 
 
 if __name__=="__main__":
