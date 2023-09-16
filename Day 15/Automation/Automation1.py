d058165ab018a29f45f728853a7b027b503a4632
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-16 17:11:54 +0530

    Update Automation1.py
    updated

--- Day 15/Automation/Automation1.py
+++ Day 15/Automation/Automation1.py
@@ -2,15 +2,25 @@ from sys import *
 
 def main():
     print("_______________Automation Script_______________")
+    print("Automation Script Name :", argv[0])
 
-    if (argv[1] == "-h"): #flag for displaying help
-        print("This automation script is used to perform addition of two  numbers")
+    if (len(argv)==2):
+        if (argv[1] == "-h" or argv[1]== "-H"): #flag for displaying help
+            print("This automation script is used to perform addition of two  numbers")
+            exit()
+
+        elif (argv[1] == "-u" or argv[1]=="-U"): #flag displaying the usage of script
+            print("Usage : Name_of_script First_Argument Second_Argument")
+            print("Example : Demo.py 11 10")
+            exit()
+
+        else:
+            print("There is no such optionn to handle")
+
+    if(len(argv) !=3):
+        print("Invalid Number of Aurguments")
         exit()
 
-    if (argv[1] == "-u"): #flag displaying the usage of script
-        print("Usage : Name_of_script First_Argument Second_Argument")
-        print("Example : Demo.py 11 10")
-        exit()
 
 if __name__=="__main__":
     main()
