78a3e44c425fe64f5761d2c77d3caee4c30e9cd4
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-18 09:12:52 +0530

    updated

--- Day 16/Automaton/script4.py
+++ Day 16/Automaton/script4.py
@@ -4,6 +4,27 @@ from sys import *
 import os
 import time
 
+def Delete_from_Directory(dirname,filename):
+    print("We are going to scan directory :", dirname)
+
+    flag = os.path.isabs(dirname)
+    if flag == False:
+        dirname = os.path.abspath(dirname)
+
+    exist = os.path.isdir(dirname)
+
+    if exist:
+        for foldername, subfoldername, fname in os.walk(dirname):
+            for file_name in fname:
+                if file_name == filename:
+                    print("File is present in current directory :", file_name)
+                    os.remove(os.path.join(foldername,file_name))
+                    print("File removed from directory")
+
+    else:
+        print("Invalid Path")
+
+
 def main():
 
     print("--------------------------Automation Script--------------------------")
@@ -15,7 +36,19 @@ def main():
 
     elif(argv[1]=="-u" or argv[1]=="-U"):
         print("Usage : Name_of_script First_aurgument Second_aurgument")
-        
+        print("Example Demo.py Python file.txt")
+        exit()
+
+    if len(argv) != 3:
+        print("Invalid Number of aurguments")
+        exit()
+    else:
+        starttime = time.time()
+
+        Delete_from_Directory(argv[1],argv[2])
+
+        endtime = time.time()
+        print("The script took time to execute :", endtime-starttime)
 
 if __name__=="__main__":
     main()
--- README.md
+++ README.md
@@ -17,3 +17,4 @@ Daily Python Coding Challenge starting from 26.08.2023
 * {Day 14 :[Detect Capital, Perfect Number, Relative Ranks]}
 * {Day 15 :[Repeated Substring pattern, Counting Bits ,Automation using Python (File Automation)]}
 * {Day 16 :[Automation Using Python]}
+* {Day 17 :[Automation Scripts]}
