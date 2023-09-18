d69c5c4db4eadaf786b9f7e74375db45d873a888
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-18 23:51:13 +0530

    Create script3.py

---
+++ Day 17/script3.py
@@ -0,0 +1,77 @@
+#remove duplicate files using script
+
+from sys import *
+import os
+import hashlib
+
+
+def hashfile(path, blocksize=1024):
+    afile = open(path,'rb')
+    hasher = hashlib.md5()
+    buffer = afile.read(blocksize)
+
+    while len(buffer)>0:
+        hasher.update(buffer)
+        buffer = afile.read(blocksize)
+    afile.close()
+
+    return hasher.hexdigest()
+
+def Checkdups(path):
+    flag = os.path.isabs(path)
+    if flag == False:
+        path = os.path.abspath(path)
+    
+    exist = os.path.isdir(path)
+    if exist:
+        dups = {}
+        for dirname, subdirname, filename in os.walk(path):
+            for file in filename:
+                f_path = os.path.join(dirname,file)
+                hash_file = hashfile(f_path)
+                
+                if hash_file in dups:
+                    dups[hash_file].append(f_path)
+                else:
+                    dups[hash_file]=[f_path]
+        return dups
+    else:
+        print("Invalid Path")
+
+def RemoveDups(dict1):
+    results = list(filter(lambda x : len(x)>1, dict1.values()))
+
+    if len(results)>0:
+        print("Duplicates found")
+        print()
+        for result in results:
+            print("Below files are identical :")
+            for sub in result:
+                print(sub)
+            print()
+
+
+        for result in results:
+            count = 0
+        
+            for sub in result:
+                count +=1
+                if count > 1:
+                    print(f"to be removed....{sub}")
+                    
+                    os.remove(sub)
+
+    else:
+        print("No duplicates found")
+
+
+def main():
+
+    if len(argv) !=2:
+        print("Invalid number of arguments")
+    else:
+        arr=Checkdups(argv[1])
+        RemoveDups(arr)
+
+if __name__=="__main__":
+    main()
