4367f190ada9d6af5f12fcf329a225609adcd738
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-23 14:20:59 +0530

    Update Namelogy.py

--- Day 21/Namelogy.py
+++ Day 21/Namelogy.py
@@ -5,8 +5,18 @@ def Numerical(string):
 
     value_dict = {1:['a','j','s'], 2:['b','k','t'], 3:['c','l','u'], 4:['d','m','v'], 5:['e','n','w'],
                   6:['f','o','x'], 7:['g','p','y'], 8:['h','q','z'], 9:['r','i']}
-    for i in string:
-        print(i)
+    string = string.lower()
+
+    key_val = []
+
+    for char in string:
+        for key, value_list in value_dict.items():
+            if char in value_list:
+                key_val.append(key)
+
+    total = sum(key_val)
+    print(total)
+
 
 
 def main():
