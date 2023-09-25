420e45b76d51e0cbc44c5f81f4e749b8901a2577
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-25 13:03:12 +0530

    Update Namelogy.py

--- Day 21/Namelogy.py
+++ Day 21/Namelogy.py
@@ -1,4 +1,14 @@
 # Provide a Numerological value of given name, life path number and destiny number of birthdate
+def digit_sum(num):
+    while num >= 10:
+        sum_digit = 0
+        while num > 0:
+            sum_digit += num % 10
+            num //= 10
+
+        num = sum_digit
+
+    return num
 
 def Numerical(string):
     print("_______________________________Your Numeroloy Report_______________________________")
@@ -15,36 +25,39 @@ def Numerical(string):
                 key_val.append(key)
 
     total = sum(key_val)
+    total = digit_sum(total)
+
     print("You Name total :",total)
 
 def Birth_Num(date):
     from datetime import datetime
 
     date = datetime.strptime(date,'%d/%m/%Y').date()
-    birthday = str(date.day)
+    birthday = int(date.day)
+    birthday = digit_sum(birthday)
 
-    birthnum = 0
-    for i in birthday:
-        birthnum += int(i)
-
-    while birthnum >= 10:
-        
-    print("Your ",birthnum)    
-    
+    print("Your Birth Number is :", birthday)
 
 
 def Life_path(date):
-    pass
+    digit = 0
 
+    for i in date:
+        if i.isdigit():
+            digit += int(i)
 
+    digit = digit_sum(digit)
+    
+    print("Your Life Path Number is :", digit)
 
 def main():
-    Name = 'Prerana Pagar'#input("Enter your name :")
-    Birthdate = '03/12/1995' #input("Enter your birthday in fotmat (dd/mm/yyyy):")
+    Name = input("Enter your name :")
+    Birthdate = input("Enter your birthday in format (dd/mm/yyyy):")
 
 
     Numerical(Name)
     Birth_Num(Birthdate)
+    Life_path(Birthdate)
 
 if __name__=="__main__":
     main()
