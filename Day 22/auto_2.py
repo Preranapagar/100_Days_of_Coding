ef71501a39bdfa4bf469a1af31d603b216ba0b80
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-26 14:46:23 +0530

    Create auto_2.py

---
+++ Day 22/auto_2.py
@@ -0,0 +1,8 @@
+# auto script which accepts directory name and two file extensions from user and rename all files with first file extension with second
+#ex Demo.txt = Demo.doc
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
