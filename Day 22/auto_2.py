cd9d8113a39f838cdde3b70ac1768e1adc55ee80
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-26 14:51:52 +0530

    Update auto_2.py

--- Day 22/auto_2.py
+++ Day 22/auto_2.py
@@ -1,8 +1,28 @@
 # auto script which accepts directory name and two file extensions from user and rename all files with first file extension with second
 #ex Demo.txt = Demo.doc
+from sys import *
+import os
 
 def main():
-    pass
+    print("-"*55,"Automation Script","-"*55)
+    print("Automation Script Name :", argv[0])
+
+    if len(argv)==2:
+        if argv[1]=='h' or argv[1]=='H':
+            print("This script use to rename the specific extenstion file from given directory")
+            exit()
+        elif argv[1]=='u' or argv[1]=='U':
+            print("Usage : Script_Name First_Aurgument Second_Aurgument Third_Aurgument")
+            print("Example : script.py Demo .txt .doc")
+            exit()
+        else:
+            print("Given aurgument is incorrect, try again with 'H' or 'U'")
+            exit()
+
+    if len(argv)==4:
+        pass
+    else:
+        print("Invalid Number of Aurguments")
 
 if __name__ == "__main__":
     main()
