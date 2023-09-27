3a6140d5494ed9182ab71f24112f6b08f6cf8847
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-27 15:32:50 +0530

    Update Auto4.py

--- Day 23/Auto4.py
+++ Day 23/Auto4.py
@@ -14,3 +14,28 @@ def Hashfile(path,blocksize=1024):
 
     obj.close()
     return hasher.hexdigest()
+
+def CheckSum(dir):
+    print("Scanning Directory :", dir)
+
+    flag = os.path.isabs(dir)
+    if flag == False:
+        dir = os.path.abspath(dir)
+
+    dups = {}
+    file_paths = {}
+
+    exist = os.path.isdir(dir)
+    if exist:
+        for Dirname, Foldername, Filename in os.walk(dir):
+            for file in Filename:
+                f_path = os.path.join(Dirname,file)
+                file_paths[file]=f_path
+                checksum = Hashfile(f_path)
+                if checksum in dups:
+                    dups[checksum].append(file)
+                else:
+                    dups[checksum]=[file]
+        return dups, file_paths
+    else:
+        print("Invalid Directory Path")
