e75cc26991baefce57e48aa040de4bb7cf648a42
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-29 19:43:42 +0530

    Create processmonitory.py

---
+++ Day 24/processmonitory.py
@@ -0,0 +1,5 @@
+#automation script which accepts time interval from user and create log file that Marvellous directory which contains
+#information of all running processes. after creating that log file send that log file through mail
+
+import os
+import time
