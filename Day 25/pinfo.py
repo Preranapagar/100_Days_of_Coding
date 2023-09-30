3da87d4f757fc5ae0bee3cc070c36f243f6ccc58
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-30 12:52:01 +0530

    Update pinfo.py

--- Day 25/pinfo.py
+++ Day 25/pinfo.py
@@ -1,5 +1,6 @@
 import psutil
 import os
+import datetime as ds
 
 def ProcessDisplay():
     listprocess = []
@@ -20,5 +21,21 @@ def CreateLog(process_list,log_dir = "Process_log"):
             os.mkdir(log_dir)
         except:
             pass
+
+    current_time = ds.datetime.now()
+    timestamp = current_time.strftime('%d_%b_%y-%H-%M-%S')
+
+    seperator = '-'*80
+
+    log_path = os.path.join(log_dir,"%sLog_%s.log"%(log_dir,timestamp))
+    log = open(log_path,'w')
+    log.write(seperator+'\n')
+    log.write("Process Logger :"+current_time+'\n')
+    log.write(seperator+'\n')
+
+    for ele in process_list:
+        log.write(ele+'\n')
+
+    log.close()
 
     
