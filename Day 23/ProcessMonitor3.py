cdca32f252402fb8c08b8836987f63df5cda3c7e
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-09-30 12:41:35 +0530

    Update ProcessMonitor3.py

--- Day 23/ProcessMonitor3.py
+++ Day 23/ProcessMonitor3.py
@@ -2,6 +2,7 @@
 
 import os
 import psutil
+import datetime
 import time
 from sys import *
 
@@ -13,9 +14,11 @@ def ProcessDisplay(log_dir='Ganesha'):
             os.mkdir(log_dir)
         except:
             pass
-
+    
+    current_time = datetime.datetime.now()
+    timestamp = current_time.strftime('%d_%b_%y-%H_%M_%S')
     seperator = '-'*80
-    log_path = os.path.join(log_dir,"GaneshaLog%s.log"%(time.ctime()))
+    log_path = os.path.join(log_dir,"GaneshaLog_%s.log"%(timestamp))
     f = open(log_path,'w')
     f.write(seperator+'\n')
     f.write("Process Logger :"+time.ctime()+'\n')
@@ -23,7 +26,7 @@ def ProcessDisplay(log_dir='Ganesha'):
 
     for proc in psutil.process_iter():
         try:
-            pinfo = proc.ac_dict(attrs=['pid','name','username'])
+            pinfo = proc.as_dict(attrs=['pid','name','username'])
             vms = proc.memory_info().vms/(1024*1024)
             pinfo['vms'] = vms
             listprocess.append(pinfo)
@@ -36,7 +39,7 @@ def ProcessDisplay(log_dir='Ganesha'):
 def main():
     print("Process Monitor to create log of all running processes in given directory")
     print("Application name :"+argv[0])
-
+    print(argv[1])
     if len(argv) != 2:
         print("Invalid Number of Aurguments")
         exit()
