a71835da4790ccec6e91f904d496f030843d93bc
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-10-04 15:08:24 +0530

    Create Create_csv.py

---
+++ Day 29/Create_csv.py
@@ -0,0 +1,54 @@
+import csv
+from sys import *
+import os
+import psutil
+
+def Process_Log():
+    listprocess = []
+    try:
+        for proc in psutil.process_iter():
+            pinfo = proc.as_dict(attrs=['pid','name','username'])
+            listprocess.append(pinfo)
+    
+    except (psutil.NoSuchProcess,psutil.AccessDenied, psutil.ZombieProcess):
+        pass
+    
+    return listprocess
+
+def Create_csv(process,dirname):
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    filename = 'process.csv'
+    path = os.path.join(dirname,filename)
+
+    with open(path,'w',newline="") as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        for row in process:
+            csv_writer.writerow(row)
+
+    print("CSV log file created successfully")
+
+
+def main():
+    print("Automation Script Name : ", argv[0])
+
+    if len(argv) !=2:
+        print("Invalid Number of arguemtns")
+        exit()
+
+    if argv[1]=='-h' or argv[1]=='-H':
+            print("This script use to create log records of running processes")
+            exit()
+
+    elif argv[1]=='-u' or argv[1]=='-U':
+        print("Usage : Script_Name First_Aurgument")
+        print("Example : script.py Demo")
+        exit()
+    
+    else:
+
+
+if __name__=="__main__":
+    main()
