6249766b6791301e7fde07e4e15b35ac04b022e9
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-10-06 10:38:47 +0530

    Update Task1.py

--- Day 30/Task1.py
+++ Day 30/Task1.py
@@ -1,4 +1,6 @@
 import os
 from sys import *
 from email.mime.multipart import MIMEMultipart
-from email.mime.
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
+
