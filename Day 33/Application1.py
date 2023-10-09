58608da9b6847bc9920f13f4e5449d382b2c147d
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-10-09 20:59:34 +0530

    Update Application1.py

--- Day 33/Application1.py
+++ Day 33/Application1.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+data = [{'Name':'PPA','Duration':3,'Fees':18500},{'Name':'Angular','Duration':4,'Fees':20000},
+        {'Name':'LB','Duration':3.2,'Fees':19500}]
+
+df = pd.DataFrame(data)
+print(df)
+
+writer = pd.ExcelWriter('Data.xlsx',engine='xlsxwriter')
+
+df.to_excel(writer,sheet_name='Sheet1')
+
+writer.save()
