0f318eb1d5f8b8f6aa3f1907e987933da967872e
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-10-12 18:37:09 +0530

    Create Predictor.py

---
+++ Day 36/Predictor.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+def PlayPredictor(data_path):
+    pass
+
+def main():
+    PlayPredictor("PlayPredictor.csv")
+
+if __name__=="__main__":
+    main()
