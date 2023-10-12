92badfdc9ab96d9da3fa4ea902bb00fe91bef6f2
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-10-12 20:50:12 +0530

    Update Predictor.py

--- Day 36/Predictor.py
+++ Day 36/Predictor.py
@@ -11,35 +11,42 @@ def PlayPredictor(data_path,values):
 
     le = LabelEncoder()
 
-    whether_en = le.fit_transform(df['Whether'])
-    temp_en = le.fit_transform(df['Temperature'])
+    features = df[['Whether','Temperature']]
+    feature_encoded = features.copy()
+    for col in features.columns:
+        feature_encoded = le.fit_transform(features[col])
     label = le.fit_transform(df['Play'])
 
-    features = list(zip(whether_en,temp_en))
-
     KNN_model = KNeighborsClassifier(n_neighbors = 3)
-    KNN_model.fit(features,label)
+    KNN_model.fit(feature_encoded,label)
 
-    Predicted = KNN_model.predict(values)
+    inputs = le.transform(values)
+    Predicted = KNN_model.predict(inputs)
     return Predicted
 
 def Inputs(W,T):
-    wether_dict = {1:'Overcast',2:'Rainy',3:'Sunny'}
-    temp_dict = {1:'Hot',2:'Cold',3:'Mild'}
-
-    result = [wether_dict[W],temp_dict[T]]
+    if W not in range(4) or T not in range(4):
+        print("Invalid input")
+        exit()
+    else:
+        wether_dict = {1:'Overcast',2:'Rainy',3:'Sunny'}
+        temp_dict = {1:'Hot',2:'Cold',3:'Mild'}
+        result = [wether_dict[W],temp_dict[T]]
 
     return result
 
 def main():
     print("Play Predictor Application")
     print("Choose the current whether between 1.Overcast 2.Rainy 3.Sunny")
-    W = int(input("Wether option"))
+    W = int(input("Wether option :"))
 
     print("Choose the current Temperature between 1.Hot 2.Cold 3.Mild")
-    T = int(input("Temperature option"))
+    T = int(input("Temperature option :"))
 
     values = Inputs(W,T)
+    
+    result =PlayPredictor("PlayPredictor.csv",values)
+    print(result)
 
 if __name__=="__main__":
     main()
