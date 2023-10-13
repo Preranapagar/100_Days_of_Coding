f5844f5e2b73c833dde9a69a1e552627320584fb
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-10-13 13:20:09 +0530

    Update Predictor.py

--- Day 36/Predictor.py
+++ Day 36/Predictor.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 def PlayPredictor(data,values):
     df = data
@@ -41,27 +42,56 @@ def Inputs(W,T):
 
     return result
 
-def CheckAccuracy(k=3):
-    pass
+def CheckAccuracy(data, k=3):
+    df = data
+    le = LabelEncoder()
+
+    wether_encoded = le.fit_transform(df['Whether'])
+    temp_encoded = le.fit_transform(df['Temperature'])
+    target = le.fit_transform(df['Play'])
+
+    Features = list(zip(wether_encoded,temp_encoded))
+
+    X_train,X_test,y_train,y_test = train_test_split(Features,target,test_size=0.2, random_state = 33)
+
+    model = KNeighborsClassifier(n_neighbors = k)
+    model.fit(X_train,y_train)
+
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracy = round(accuracy * 100,2)
+
+    print(f"Accuracy score of model with {k} neighbors is {accuracy} %")
+    
 
 
 def main():
     print("Play Predictor Application")
-    print("Choose the current whether between 1.Overcast 2.Rainy 3.Sunny")
-    W = int(input("Wether option :"))
-
-    print("Choose the current Temperature between 1.Hot 2.Cold 3.Mild")
-    T = int(input("Temperature option :"))
-
+    
+    mode = int(input("Choose activity to perform between 1.PlayPredictor 2.AccuracyCheck :"))
     df = pd.read_csv("PlayPredictor.csv")
-    values = Inputs(W,T)
     
-    result =PlayPredictor(df,values)
+    if mode == 1:
+
+        print("Choose the current whether between 1.Overcast 2.Rainy 3.Sunny")
+        W = int(input("Wether option :"))
+
+        print("Choose the current Temperature between 1.Hot 2.Cold 3.Mild")
+        T = int(input("Temperature option :"))
+        values = Inputs(W,T)
+        
+        result =PlayPredictor(df,values)
     
-    if result[0]==1:
-        print("Yes, you can play")
-    elif result[0]==0:
-        print("No, yo cant play")
+        if result[0]==1:
+            print("Yes, you can play")
+        elif result[0]==0:
+            print("No, yo cant play")
+    elif mode == 2:
+        neighbors = int(input("Enter the number of neighbors :"))
+        CheckAccuracy(df,neighbors)
+
+    else:
+        print("Invalid input")
 
 if __name__=="__main__":
     main()
