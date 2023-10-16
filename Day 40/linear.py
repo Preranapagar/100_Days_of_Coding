2a8cab372d8e67891bd35be85201b5fe8b169bdb
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2023-10-16 22:15:43 +0530

    Create linear.py

---
+++ Day 40/linear.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def HeadBrainPredictor():
+
+    data = pd.read_csv("HeadBrain.csv")
+
+    print("Size of dataset is:", data.shape)
+
+    X = data['Head Size(cm^3)'].values
+    Y = data['Brain Weight(grams)'].values
+
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+
+    n = len(X)
+
+    numerator = 0
+    denomenator = 0
+
+    for i in range(n):
+        numerator += (X[i]-mean_x)*(Y[i]-mean_y)
+        denomenator += (X[i]-mean_x)**2
+
+    m =  numerator / denomenator
+
+    c = mean_y - (m*mean_x)
+
+    print("slope of regression line :", m)
+    print("Y intercept of regression line :", c)
+
+    max_x = np.max(X)+100
+    min_x = np.min(X)-100
+
+    x = np.linspace(min_x,max_x,n)
+
+    y = c + m * x
+
+    plt.plot(x,y, color ='#58b97', label = 'Regression Line')
+    plt.scatter(X,Y, color ='#ef5423', label = 'scatter plot')
+    plt.xlabel('Head Size in cm3')
+    plt.ylabel('Brain size in gram')
+
+    plt.legend()
+    plt.show()
+
+    ss_t = 0
+    ss_r = 0
+
+    for i in range(n):
+        y_pred = c + m * X[i]
+        ss_t += (Y[i] - mean_y)**2
+        ss_r += (Y[i] - y_pred)**2
+
+    r2 = 1-(ss_r/ss_t)
+    print(r2)
+
+def main():
+    print("Supervise Machine learnig")
+    print("Linear Regression algorithm")
+
+    HeadBrainPredictor()
+
+if __name__=="__main__":
+    main()
