05b718979e92047edaf5c64d596ee8c448e8be14
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-02-07 17:33:35 +0530

    Update matrix.py

--- Day 52/matrix.py
+++ Day 52/matrix.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 def create_matrics(nums):
-    pass
+    
+    result = np.array(nums).reshape(3,3)
+    print(result)
 
 def main():
-    create_matrics()
+    
+    list1 = [1,2,3,4,5,6,7,8,9]
+
+    create_matrics(list1)
 
 
 if __name__=='__main__':
