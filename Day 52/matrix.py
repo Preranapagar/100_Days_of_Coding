2c8e4cb3a059c09abdbb887684c5207335ba359d
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-02-09 10:56:52 +0530

    Update matrix.py

--- Day 52/matrix.py
+++ Day 52/matrix.py
@@ -1,16 +1,29 @@
 import pandas as pd
 import numpy as np
 
-def create_matrics(nums):
+def read_matrics():
+    df = pd.read_excel('matrix.xlsx')
+    matrix_data = df.values
+    print(matrix_data)
+
+def create_matrics(nums,rows,cols):
     
-    result = np.array(nums).reshape(3,3)
-    print(result)
+    result = np.array(nums).reshape(rows,cols)
+    df = pd.DataFrame(result)
+
+    file_name = 'matrix.xlsx'
+    df.to_excel(file_name, index=False, header=False)
+
+    print("Excel file is created")
 
 def main():
     
     list1 = [1,2,3,4,5,6,7,8,9]
+    r = 3
+    c = 3
 
-    create_matrics(list1)
+    # create_matrics(list1,r,c)
+    read_matrics()
 
 
 if __name__=='__main__':
